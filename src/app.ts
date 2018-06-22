45b7c9ce9c199a05dc98913bbf27862c2847a521
Will Trott <will@linuxacademy.com>
2018-06-22 09:17:28 -0500

    Minor updates

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log('Hello TypeScript!');
+const pizza = {
+    name: 'Pepperoni',
+    price: 15,
+    getName() {
+        return this.name;
+    },
+};
+
+console.log(pizza.getName())
+
+const toppings = ['pepperoni'];
+
+const order = {pizza, toppings};
+
+console.log(order)
